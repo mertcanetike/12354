9cad4b46884b60fd7121709cf2b2fea48ba2b7cc
mertcanetike <93284438+mertcanetike@users.noreply.github.com>
2021-11-11 20:57:47 +0300

    Add files via upload

---
+++ arrays 9.11.2021/arrays 9.11.2021/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace arrays_9._11._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] sayilar = new int[4];
+
+            sayilar[0] = 10;
+            sayilar[1] = 20;
+            sayilar[2] = 30;
+            sayilar[3] = 40;
+
+            for (int i = 0; i<sayilar.Length; i++)
+            {
+                Console.WriteLine(sayilar[i]);
+            }
+
+
+        }
+    }
+}
