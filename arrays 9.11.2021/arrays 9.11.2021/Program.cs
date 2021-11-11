9ebb1e6456c6b735c3288988d6ca8e6c69807899
mertcanetike <93284438+mertcanetike@users.noreply.github.com>
2021-11-11 20:58:49 +0300

    Update Program.cs

--- arrays 9.11.2021/arrays 9.11.2021/Program.cs
+++ arrays 9.11.2021/arrays 9.11.2021/Program.cs
@@ -7,12 +7,14 @@ namespace arrays_9._11._2021
         static void Main(string[] args)
         {
 
-            int[] sayilar = new int[4];
+            int[] sayilar = new int[5];
 
             sayilar[0] = 10;
             sayilar[1] = 20;
             sayilar[2] = 30;
             sayilar[3] = 40;
+            sayilar[4] = 40;
+            
 
             for (int i = 0; i<sayilar.Length; i++)
             {
